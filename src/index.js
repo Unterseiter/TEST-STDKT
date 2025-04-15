3e743bd6cd728f57d655eab52e8f4204fab0e001
Unterseiter <wwwandromedix@gmail.com>
2025-04-15 22:44:01 +0300

    v1.61

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 import '../src/styles/main.scss';
+import Swiper from 'swiper';
+import 'swiper/css';
 
+class Carousel {
+    constructor(container) {
+        this.container = container;
+        this.slides = container.querySelector('.carousel__container');
+        this.items = Array.from(container.querySelectorAll('.carousel-item'));
+        this.currentIndex = 0;
+        this.itemWidth = this.items[0].offsetWidth + 20; // включая gap
+
+        this.initEvents();
+    }
+
+    initEvents() {
+        this.container.querySelector('.carousel__button--prev')
+            .addEventListener('click', () => this.move(-1));
+        
+        this.container.querySelector('.carousel__button--next')
+            .addEventListener('click', () => this.move(1));
+    }
+
+    move(direction) {
+        this.currentIndex = Math.max(
+            0, 
+            Math.min(this.currentIndex + direction, this.items.length - 3)
+        );
+        
+        this.slides.style.transform = `translateX(-${this.currentIndex * this.itemWidth}px)`;
+    }
+}
+
+// Инициализация карусели
+new Carousel(document.querySelector('.carousel'));
--- webpack.config.js
+++ webpack.config.js
@@ -22,15 +22,23 @@ module.exports = (env, argv) => {
       static: './dist',
       hot: true,
       open: true,
+      historyApiFallback: true
     },
 
     module: {
       rules: [
+        // Добавлено правило для CSS из node_modules
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+          include: /node_modules/
+        },
+
         {
           test: /\.svg$/i,
           type: 'asset/resource',
           generator: {
-            filename: 'assets/images/[name][ext][query]', // Сохранить оригинальные имена
+            filename: 'assets/images/[name][ext][query]',
           },
         },
 
@@ -75,6 +83,7 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: './src/index.html',
         filename: 'index.html',
+        minify: isProduction
       }),
     ],
 
@@ -83,6 +92,13 @@ module.exports = (env, argv) => {
         new CssMinimizerPlugin(),
         new TerserPlugin(),
       ],
+    },
+
+    resolve: {
+      extensions: ['.js', '.json'],
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     },
 
     devtool: isProduction ? 'source-map' : 'inline-source-map',
